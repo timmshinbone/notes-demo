6e5f62c21162cd5cdfbdf0f8f27173e539e0537a
Timm Schoenborn <timmschoenborn@gmail.com>
2023-06-27 16:04:37 -0400

    adds cors for deploy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
+const cors = require('cors')
 // Always require and configure near the top
 require('dotenv').config();
 // Connect to the database
@@ -16,6 +17,11 @@ app.use(express.json());
 // to serve from the production 'build' folder
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
+app.use(
+  cors({
+    origin: ['http://localhost:3000', 'https://coolpeas-notes.onrender.com']
+  })
+)
 
 // Middleware to check and verify a JWT and
 // assign the user object from the JWT to req.user
